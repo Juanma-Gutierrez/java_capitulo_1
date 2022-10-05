a455f4e4ffc594a646de98f9ec0d94e30be802d3
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-05 14:18:50 +0200

    feat: add Ejercicio 07

---
+++ Ejercicio07.java
@@ -0,0 +1,34 @@
+/*
+* Exercise: 8¡7
+* Created: 05/10/2022 14:12
+* 
+* Author: Juan Manuel Gutiérrez
+* https://github.com/Juanma-Gutierrez
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio07 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce la altura de la pirámide: ");
+        int alt = sc.nextInt();
+        if (alt <= 0) {
+            System.out.print("Ha introducido una altura no válida: ");
+        } else {
+            for (int i = 1; i <= alt; i++) {
+                for (int j = 1; j <= alt - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= i * 2 - 1; j++) {
+                    if (j <= 1 || j >= i * 2 - 1 || i == alt)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println("");
+            }
+        }
+        sc.close();
+    }
+}
