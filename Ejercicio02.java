88387f518204c8729f4f91c39bb3654b1c77aebf
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-05 13:25:57 +0200

    feat: add Ejercicio 02

---
+++ Ejercicio02.java
@@ -0,0 +1,15 @@
+/*
+* Ejercicio 2
+* Created: 05/10/2022 13:19
+* 
+* Author: Juan Manuel Gutiérrez
+* https://github.com/Juanma-Gutierrez
+*/
+
+public class Ejercicio02 {
+    public static void main(String args[]) {
+        System.out.println("Juan Manuel Gutierrez");
+        System.out.println("C/ Frederick Termann, s/n - PTA Malaga - Campanillas - Malaga");
+        System.out.println("952.11.22.33");
+    }
+}
