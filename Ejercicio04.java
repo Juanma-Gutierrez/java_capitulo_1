bb18282888e050c1065fd0697a6acae3ca95e062
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-05 13:49:00 +0200

    feat: add Ejercicio 04

---
+++ Ejercicio04.java
@@ -0,0 +1,24 @@
+/*
+* Exercise: 4
+* Created: 05/10/2022 13:36
+* 
+* Author: Juan Manuel Gutiérrez
+* https://github.com/Juanma-Gutierrez
+*/
+
+public class Ejercicio04 {
+    public static void main(String args[]) {
+        System.out.println("+-------------+-----------+-----------+-----------+-----------+-----------+");
+        System.out.println("|    Hora     |   Lunes   |   Martes  | Miércoles |   Jueves  |  Viernes  |");
+        System.out.println("+-------------+-----------+-----------+-----------+-----------+-----------+");
+        System.out.println("| 08:15-09:15 | Programa. | Sist.Inf. | Ent.Desa. | Programa. | Form.Or.L |");
+        System.out.println("| 09:15-10:15 | Programa. | Sist.Inf. | Ent.Desa. | Programa. | Form.Or.L |");
+        System.out.println("| 10:15-11:15 | Len.Marc. | Sist.Inf. | Programa. | Len.Marc. | Form.Or.L |");
+        System.out.println("| 11:45-12:45 | Len.Marc. | Programa. | Programa. | Len.Marc. | Sist.Inf. |");
+        System.out.println("| 12:45-13:45 |   BBDD    | Programa. |   BBDD    |   BBDD    | Sist.Inf. |");
+        System.out.println("| 13:45-14:45 |   BBDD    | Ent.Desa. |   BBDD    |   BBDD    | Sist.Inf. |");
+        System.out.println("+-------------+-----------+-----------+-----------+-----------+-----------+");
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # java_capitulo_1
 
+## Ejercicio 1
 
+Escribe una programa que muestre tu nombre por pantalla.
+
+## Ejercicio 2
+
+Modifica el programa anterior para que además se muestre tu dirección y
+tu número de teléfono. Asegúrate de que los datos se muestran en líneas
+separadas.
+
+## Ejercicio 3
+
+Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su
+correspondiente traducción al castellano. Las palabras deben estar distribuidas
+en dos columnas y alineadas a la izquierda
