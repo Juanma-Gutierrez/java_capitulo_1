2f9cec77e465f42bad538609d66731333d8b64c3
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-05 14:35:22 +0200

    feat: add Ejercicio 09

---
+++ Ejercicio09.java
@@ -0,0 +1,20 @@
+/*
+* Exercise: 9
+* Created: 05/10/2022 14:25
+* 
+* Author: Juan Manuel Gutiérrez
+* https://github.com/Juanma-Gutierrez
+*/
+
+public class Ejercicio09 {
+    public static void main(String args[]) {
+        System.out.println("    ███████████    ");
+        System.out.println("  ██▒▒▒▒▒▒▒▒▒▒▒██  ");
+        System.out.println("██▒▒▒▒█▒▒▒▒▒█▒▒▒▒██");
+        System.out.println("█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█");
+        System.out.println("█▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒█");
+        System.out.println("██▒▒▒█████████▒▒▒██");
+        System.out.println("  ██▒▒▒▒▒▒▒▒▒▒▒██  ");
+        System.out.println("    ███████████    ");
+    }
+}
